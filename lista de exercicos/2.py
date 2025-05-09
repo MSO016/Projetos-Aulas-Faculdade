62acb137216e387b33aed38bb5f7430e310e1163
MSO016 <mateussocontato@gmail.com>
2025-05-09 01:49:55 -0300

    Delete lista de exercicos directory

--- lista de exercicos/2.py
+++
@@ -1,9 +0,0 @@
-numeros = []
-
-for i in range(10):
-    num = float(input(f"Digite o {i+1}º número real: "))
-    numeros.append(num)
-
-print("Os números na ordem inversa são:")
-for num in reversed(numeros):
-    print(num)
--- lista de exercicos/9.py
+++
@@ -1,9 +0,0 @@
-A = []
-soma_quadrados = 0
-
-for i in range(10):
-    num = int(input(f"Digite o {i+1}º número inteiro: "))
-    A.append(num)
-    soma_quadrados += num ** 2  # Soma do quadrado do número
-
-print("A soma dos quadrados dos elementos do vetor A é:", soma_quadrados)
